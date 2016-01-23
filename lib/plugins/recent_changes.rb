eb556b0a249b4f727a528a16ae40f4088c70645b
elifoster <elifosterwy@gmail.com>
2016-01-22 20:05:02 -0800

    :arrow_up: Update to Ruby 2.3 and HTTPClient 2.7

--- lib/plugins/recent_changes.rb
+++ lib/plugins/recent_changes.rb
@@ -5,6 +5,12 @@ module Plugins
   class RecentChanges
     include Cinch::Plugin
 
+    NO_COMMENT = 'No comment :('.freeze
+    NEW = ' New!'.freeze
+    MINOR = ' Minor!'.freeze
+    BOT = ' Bot!'.freeze
+    PATROLLED = '!! '.freeze
+
     @last_time = nil
 
     timer(15, method: :execute)
@@ -23,18 +29,17 @@ module Plugins
       return if rc.size == 0
 
       rc.each do |log|
-        message = "[#{log[:type].capitalize} #{log[:revid]}] #{log[:title]} " \
-                  "(#{log[:user]}) "
+        message = "[#{log[:type].capitalize} #{log[:revid]}] #{log[:title]} (#{log[:user]}) "
         if log[:comment].empty?
-          message << 'No comment :('
+          message << NO_COMMENT
         else
           message << log[:comment]
         end
         message << " (#{log[:diff_length]})"
-        message << ' New!' if log[:new]
-        message << ' Minor!' if log[:minor]
-        message << ' Bot!' if log[:bot]
-        message.prepend('!! ') if log[:patrolled]
+        message << NEW if log[:new]
+        message << MINOR if log[:minor]
+        message << BOT if log[:bot]
+        message.prepend(PATROLLED) if log[:patrolled]
         RecentChangesBot::IRC.channels.each do |channel|
           channel.send(message)
         end
